85355bbf41e45d7d4047fb31fb11b837e7c9a288
DevAffairs <devaffairs.com@gmail.com>
2025-02-09 01:12:58 +0530

    admin config
    admin config update

--- Client/app/services/appConfig.ts
+++ Client/app/services/appConfig.ts
@@ -1,7 +1,7 @@
 const appConfig = {
     "API_BASE_URL": "http://localhost:3000/",
     "CLIENT_API_BASE_URL": "http://localhost:3000/",//"../",
-    "Admin_Site_Url": "http://localhost:4200/",
+    "Admin_Site_Url": "http://localhost:4200/app/",
     "Site_Url": "https://landtreat.com/",
     "production": false
 }
