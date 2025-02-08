b7d004eb7e6b053599767059fb0cf59dc710972c
DevAffairs <devaffairs.com@gmail.com>
2025-02-08 22:52:04 +0530

    Fallback loading added
    Fallback loading added

--- Client/app/library/page.tsx
+++ Client/app/library/page.tsx
@@ -4,9 +4,7 @@ import LibraryClient from './LibraryClient'; // Client Component
 export default function Library() {
     return (
         <div>
-            <Suspense fallback={<div>Loading search results...</div>}>
-                <LibraryClient />
-            </Suspense>
+            <LibraryClient />
         </div>
     );
 }
